e24044ca95c0ead25d5691355f33bdc102c11e75
Daniel Li <164278571@qq.com>
2020-09-15 11:04:53 +0800

    不断修改用于搭建框架

--- src/main/java/com/awesome/benxiaojia/BenxiaojiaApplication.java
+++ src/main/java/com/awesome/benxiaojia/BenxiaojiaApplication.java
@@ -2,8 +2,7 @@ package com.awesome.benxiaojia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class BenxiaojiaApplication {
--- src/main/java/com/awesome/benxiaojia/mbg/mapper/UmsAdminMapper.java
+++ src/main/java/com/awesome/benxiaojia/mbg/mapper/UmsAdminMapper.java
@@ -4,6 +4,7 @@ import com.awesome.benxiaojia.model.UmsAdmin;
 import java.util.List;
 
 public interface UmsAdminMapper {
+
     int deleteByPrimaryKey(Long id);
 
     int insert(UmsAdmin record);
@@ -15,4 +16,5 @@ public interface UmsAdminMapper {
     int updateByPrimaryKey(UmsAdmin record);
 
     UmsAdmin selectByName(String username);
+
 }
